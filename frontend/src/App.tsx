fdabf05b3c708b0233c658c0c59ed5757a2db3e7
Justin John <justinj9805@gmail.com>
2024-02-17 11:44:16 -0500

    homepage rough layout done plus navbar - rough

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Nav from './shared/navbar'
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import Home from './components/home';
+import Profile from './components/profile';
+import Jobs from './components/jobs';
 
 function App() {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/jobs" element={<Jobs />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ["./src/**/*.{html,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors:{
+        //define some hex colors here
+        'background': '#0F172A'
+      }
+    },
   },
   plugins: [],
 }
