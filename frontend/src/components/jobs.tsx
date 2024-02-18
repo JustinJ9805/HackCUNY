890ae7f2c0b2c3a1ce00727b5d5797b158085314
Justin John <justinj9805@gmail.com>
2024-02-17 22:53:15 -0500

    create app routing, added some small changes

--- frontend/src/components/jobs.tsx
+++ frontend/src/components/jobs.tsx
@@ -1,6 +1,31 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Link} from 'react-router-dom';
 
-const jobs = () => {
+let testPost  = {
+  title: 'test',
+  description: 'testing this out'
+}
+
+const Jobs: React.FC = () => {
+  const [posts, setPosts] = useState<any[]>([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      try{
+        const response = await axios.get('http://localhost:5000/api/posts');
+        setPosts(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getPosts();
+  }
+  , []);
+
+  console.log(posts);
+  
   return (
     <div className='pt-2 mx-4 my-2'>
       <div className='flex justify-around'>
@@ -19,23 +44,31 @@ const jobs = () => {
             </select>
           </div>
           <button className='bg-blue-500 text-white p-1 px-6 rounded-lg ml-2'>Search</button>
-        </div>
-      </div>
-      <div className='p-8 py-2 bg-slate-800 rounded-lg mt-2'>
-        {/* some fake posting in divs  */}
+          </div>
+          </div>
+          <div className='p-8 py-2 bg-slate-800 rounded-lg mt-2'>
+          {posts.map((post: any, index) => {
+            return (
+              <div key={index} className='my-2 mx-4 p-4 bg-slate-700 rounded-lg w-full'>
+                <h1 className='text-white'>{post.title}</h1>
+                <p className='text-white'>{post.description}</p>
+                <Link to={{ pathname: '/createApplication', search: `?title=${post.title}&description=${post.description}` }}>
+            <button className='bg-blue-500 text-white p-1 px-6 rounded-lg'>Apply</button>
+          </Link>
+              </div>
+            )
+          })}
         <div className='my-2 mx-4 p-4 bg-slate-700 rounded-lg w-full'>
           <h1 className='text-white'>Title</h1>
           <p className='text-white'>Description</p>
-          <button className='bg-blue-500 text-white p-1 px-6 rounded-lg'>Apply</button>
+          <Link to={{ pathname: '/createApplication', search: `?title=${testPost.title}&description=${testPost.description}` }}>
+            <button className='bg-blue-500 text-white p-1 px-6 rounded-lg'>Apply</button>
+          </Link>
         </div>
-        <div className='my-2 mx-4 p-4 bg-slate-700 rounded-lg w-full'>
-          <h1 className='text-white'>Title</h1>
-          <p className='text-white'>Description</p>
-          <button className='bg-blue-500 text-white p-1 px-6 rounded-lg'>Apply</button>
-        </div>
+        
       </div>
   </div>
   )
 }
 
-export default jobs
+export default Jobs
