a7abf34995cb673b14ec08b3680f1ae8378699e8
Taiji <Hashimototaiji@gmail.comm>
2024-02-19 10:36:03 -0500

    revised profile page

--- frontend/src/components/profile.tsx
+++ frontend/src/components/profile.tsx
@@ -32,8 +32,8 @@ const Profile = () => {
           <h1 className='text-white'>First Name</h1>
           <input
             type='text'
-            className='bg-white-500 text-black p-1 px-6 rounded-lg cursor-text outline-none'
-            placeholder='Type your First name'
+            className='bg-white-500 text-black p-1 px-6 rounded-lg cursor-text outline-none mr-2'
+            placeholder='First name'
           />
           <button className="bg-blue-400 hover:bg-blue-600 text-white text-sm font-bold rounded px-2 py-1">
               Update
@@ -44,8 +44,8 @@ const Profile = () => {
           <h1 className='text-white'>Last Name</h1>
           <input
             type='text'
-            className='bg-white-500 text-black p-1 px-6 rounded-lg cursor-text outline-none'
-            placeholder='Type your Last name'
+            className='bg-white-500 text-black p-1 px-6 rounded-lg cursor-text outline-none mr-2'
+            placeholder='Last name'
           />
           <button className="bg-blue-400 hover:bg-blue-600 text-white text-sm font-bold rounded px-2 py-1">
               Update
@@ -55,7 +55,7 @@ const Profile = () => {
         <div className='my-2 mx-4 p-4 bg-slate-700 rounded-lg w-full'>
           <h1 className='text-white'>Bio </h1>
           <textarea
-            className='bg-white-500 text-black p-3 px-6 rounded-lg cursor-text outline-none resize-none'
+            className='bg-white-500 text-black p-3 px-6 rounded-lg cursor-text outline-none resize-none mr-2'
             placeholder='Write a little about yourself & interests'
             rows={4} // Adjust the number of rows as needed
           ></textarea>
@@ -67,8 +67,8 @@ const Profile = () => {
           <h1 className='text-white'>Education</h1>
           {/* textbox for schools */}
           <textarea
-            className='bg-white-500 text-black p-3 px-6 rounded-lg cursor-text outline-none resize-none'
-            placeholder='Enter your school name'
+            className='bg-white-500 text-black p-3 px-6 rounded-lg cursor-text outline-none resize-none mr-2'
+            placeholder='Enter school name'
             rows={1} // Adjust the number of rows as needed
           ></textarea>
             <button className="bg-blue-400 hover:bg-blue-600 text-white text-sm font-bold rounded px-2 py-1">
@@ -78,7 +78,7 @@ const Profile = () => {
         <div className='my-2 mx-4 p-4 bg-slate-700 rounded-lg w-full'>
           <h1 className='text-white'>Position </h1>
           <textarea
-            className='bg-white-500 text-black p-3 px-6 rounded-lg cursor-text outline-none resize-none'
+            className='bg-white-500 text-black p-3 px-6 rounded-lg cursor-text outline-none resize-none mr-2'
             placeholder='Current position at your current workplace'
             rows={2} // Adjust the number of rows as needed
           ></textarea>
@@ -89,8 +89,8 @@ const Profile = () => {
         <div className='my-2 mx-4 p-4 bg-slate-700 rounded-lg w-full'>
           <h1 className='text-white'>Skills </h1>
           <textarea
-            className='bg-white-500 text-black p-3 px-6 rounded-lg cursor-text outline-none resize-none'
-            placeholder='List your skills and separate them with a ,'
+            className='bg-white-500 text-black p-3 px-6 rounded-lg cursor-text outline-none resize-none mr-2'
+            placeholder='List skills and separate with a comma'
             rows={4} // Adjust the number of rows as needed
           ></textarea>
           <button className="bg-blue-400 hover:bg-blue-600 text-white text-sm font-bold rounded px-2 py-1">
