4b860f609c2d58747537c1e6c6c8bbf58d7ed01f
Justin John <justinj9805@gmail.com>
2024-03-03 19:03:54 -0500

    cleaned up backend routing and connecting to db

--- express/server.js
+++ express/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const knexConfig = require('./knexfile');
-const Knex = require('knex');
+
+const Listings = require('./endpoints/listings');
 
 dotenv.config();
 const app = express();
@@ -11,26 +11,8 @@ const port = process.env.PORT || 3002;
 app.use(express.json());
 app.use(cors());
 
+app.get("/listings", Listings);
+
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
-
-const connectToDB = async (req, res, callback) => {
-  const knex = Knex(knexConfig.development);
-  try {
-    await callback(knex, req, res);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
-  } finally {
-    knex.destroy();
-  }
-};
-
-app.get('/listings', async (req, res) => {
-  console.log("endpoint hit")
-  await connectToDB(req, res, async (knex, req, res) => {
-    const listings = await knex('listings').select('*');
-    res.json(listings);
-  });
+    console.log(`App listening at http://localhost:${port}`);
 });
