9541c904d92da4fa0e1a8bd64cda329f1da662d6
Taiji <Hashimototaiji@gmail.comm>
2024-03-18 12:31:44 -0400

    Created a landingpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import Home from './components/home';
 import Profile from './components/profile';
 import Jobs from './components/jobs';
+import Landing from './components/landingPage'
 import SignIn from './components/signIn';
 import SignUp from './components/signUp';
 import CreateApplication from './components/createApplication';
@@ -30,12 +31,14 @@ function App() {
           <Route path="/" element={<Home columnStatus={column} />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/jobs" element={<Jobs />} />
+          <Route path='/landing' element={<Landing />} />
           <Route path="/signIn" element={<SignIn/>} />
           <Route path="/signUp" element={<SignUp/>} />
           <Route path="/createApplication" element={<CreateApplication/>} />
           <Route path="/createPost" element={<CreatePost/>} />
         </Routes>
       </div>
+      {/* footer */}
     </BrowserRouter>
   );
 }
